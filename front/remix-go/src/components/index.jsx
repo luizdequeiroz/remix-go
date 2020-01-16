c20f330d1062bde80cfa38c121a457afd652f254
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2020-01-16 15:35:20 -0300

    criando header do template

--- front/remix-go/src/components/index.jsx
+++ front/remix-go/src/components/index.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import routes from "./routes";
 
-import routes from './routes';
-
-function Template({ children }){
-    return (
-        <div className="container-fluid">
-            <ul>
-                {routes.filter(route => route.isMenuItem).map(route => 
-                    <Link className="btn" to={route.path}><i>{route.icon}</i> {route.name}</Link>
-                )}
-            </ul>
-            {children}
+function Template({ children }) {
+  return (
+    <>
+      <div className="w3_navigation">
+        <div className="container">
+          <nav className="navbar navbar-default">
+            <div className="navbar-header navbar-left">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <div className="w3_navigation_pos">
+                <h1><Link to="/" title="Remix Go">Remix Go</Link></h1>
+              </div>
+            </div>
+            <div className="collapse navbar-collapse navbar-right" id="menu-collapse">
+              <nav className="cl-effect-5" id="cl-effect-5">
+                <ul className="nav navbar-nav">
+                  {routes.filter(route => route.isMenuItem).map((route, index) => (
+                    <li>
+                      <Link key={index} className={index === 0 ? "active" : "scroll"} to={route.path}>
+                        <span data-hover={route.name}>{route.icon} {route.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </nav>
         </div>
-    );
+      </div>
+      {children}
+    </>
+  );
 }
 
 export default Template;
--- front/remix-go/src/components/routes.jsx
+++ front/remix-go/src/components/routes.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { FaFile, FaHome, FaCheckCircle } from 'react-icons/fa';
+import React from "react";
+import { FaFile, FaHome, FaCheckCircle } from "react-icons/fa";
 
-function RouteOne(){
-    return "Route One";
+function RouteOne() {
+  return "Route One";
 }
 
-function RouteTwo(){
-    return "Route Two";
+function RouteTwo() {
+  return "Route Two";
 }
 
-function RouteThree(){
-    return "Route Three"
+function RouteThree() {
+  return "Route Three";
 }
 
 export default [
-    {
-        exact: true,
-        path: '/',
-        icon: <FaHome />,
-        name: 'Rota Um',
-        component: RouteOne,
-        isMenuItem: true
-    },
-    {
-        path: '/route-two',
-        icon: <FaFile />,
-        name: 'Rota Dois',
-        component: RouteTwo,
-        isMenuItem: true
-    },
-    {
-        path: '/route-three',
-        icon: <FaCheckCircle />,
-        name: 'Rota Três',
-        component: RouteThree,
-        isMenuItem: false
-    }
+  {
+    exact: true,
+    path: "/",
+    icon: <FaHome />,
+    name: "Rota Um",
+    component: RouteOne,
+    isMenuItem: true
+  },
+  {
+    path: "/route-two",
+    icon: <FaFile />,
+    name: "Rota Dois",
+    component: RouteTwo,
+    isMenuItem: true
+  },
+  {
+    path: "/route-three",
+    icon: <FaCheckCircle />,
+    name: "Rota Três",
+    component: RouteThree,
+    isMenuItem: false
+  }
 ];
